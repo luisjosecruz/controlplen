be41a315788350d0db954bdff5fc6dd295d30fc3
luisjosecruz <luisjosecruzmart@gmail.com>
2022-02-25 17:30:28 -0600

    cambiando la forma del modal para reutilizarlo

--- frontend/assets/scripts/interactive.js
+++ frontend/assets/scripts/interactive.js
@@ -37,8 +37,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let closeButton = document.querySelector("#close-button");
     let openButton = document.querySelector(".open-button");
 
-    
-
     closeButton.addEventListener("click", function() {
         modal.classList.toggle("closed");
         modalOverlay.classList.toggle("closed");
@@ -47,6 +45,105 @@ document.addEventListener("DOMContentLoaded", function(event) {
     openButton.addEventListener("click", function() {
         modal.classList.toggle("closed");
         modalOverlay.classList.toggle("closed");
+    });
+
+    // create project
+    let createProject = document.getElementById("createProject");
+    let modalData = document.getElementById('modal-data');
+
+    createProject.addEventListener("click", () => {
+        let data = `
+                    <div class="modal-header">
+                        <p class="modal-title">Crear nuevo proyecto</p>
+                    </div>
+                    <div class="modal-content">
+                        <form id="formCreateProject" class="modal-form">
+                            <div class="form-left">
+                                <div class="form-group">
+                                    <label for="project-name">Nombre:</label>
+                                    <input class="input-project" id="project-name" type="text" placeholder="Proyecto" autocomplete="off">
+                                </div>
+                                <div class="form-group">
+                                    <textarea name="project-description" id="project-description" cols="10" rows="2" placeholder="Descripción del proyecto"></textarea>
+                                </div>
+                                <div class="form-group select-tags">
+                                    <label for="inputGoal">Metas</label>
+                                    <div class="create-goals">
+                                        <input class="input-goal" id="inputGoal" type="text" placeholder="Crear metas" autocomplete="off">
+                                        <a id="createGoal" class="add-goal">+</a>
+                                    </div>
+                                    <div class="create-goals">
+                                        <div class="create-goals-list"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-right">
+                                <div class="form-right-title">
+                                    <p>Atributos</p>
+                                    <span class="lj lj-bookmark"></span>
+                                </div>
+                                <div class="form-group">
+                                    <label class="attributes" for="project-status">Estado</label>
+                                    <select name="project-status" id="project-status">
+                                        <option disabled="disabled" selected value="0">Estado</option>
+                                        <option value="Pendiente">Pendiente</option>
+                                        <option value="En progreso">Inactivo</option>
+                                        <option value="Completado">Completado</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label class="attributes" for="project-start-date">Inicio</label>
+                                    <input type="date" name="project-start-date" id="project-start-date" value="<?php echo date("Y-m-d");?>">
+                                </div>
+                                <div class="form-group">
+                                    <label class="attributes" for="project-end-date">Fin</label>
+                                    <input type="date" name="project-end-date" id="project-end-date" >
+                                </div>
+                                <div class="form-group select-tags">
+                                    <label class="attributes" for="project-tags">Etiquetas</label>
+                                    <div class="create-goals">
+                                        <input class="input-goal" id="inputTag" type="text" placeholder="Agregar etiquetas" autocomplete="off">
+                                        <a id="createTag" class="add-goal">+</a>
+                                    </div>
+                                    <div class="create-goals">
+                                        <div class="create-tags-list"></div>
+                                    </div>
+                                </div>
+                                <input class="form-btn" id="saveObjective" type="submit" value="Guardar">
+                            </div>
+                        </form>
+                    </div>
+        `;
+
+        modalData.innerHTML = data;
+    });
+
+    // create goals (modal add project)
+    let addGoal = document.getElementById("createGoal");
+    let inputGoal = document.getElementById("inputGoal");
+    let goalsList = document.querySelector(".create-goals-list");
+    
+    addGoal.addEventListener("click", () => {
+        const input = document.createElement('input');
+        input.setAttribute("type", "text");
+        input.setAttribute("readonly", "true");
+        input.setAttribute("value", "— " + inputGoal.value);
+        goalsList.appendChild(input);
+        inputGoal.value = "";
+    });
+
+    // create project tags (modal add project)
+    let addTag = document.getElementById("createTag");
+    let inputTag = document.getElementById("inputTag");
+    let tagList = document.querySelector(".create-tags-list");
+    
+    addTag.addEventListener("click", () => {
+        const input = document.createElement('input');
+        input.setAttribute("type", "text");
+        input.setAttribute("readonly", "true");
+        input.setAttribute("value", "— " + inputTag.value);
+        tagList.appendChild(input);
+        inputTag.value = "";
     });
 
 });
--- frontend/views/start.php
+++ frontend/views/start.php
@@ -12,7 +12,7 @@
                 <div class="dashboard">
                     <div class="dashboard-top">
                         <p class="dashboard-top__title options">
-                            <button id="open-button" class="open-button">
+                            <button id="createProject" class="open-button">
                                 <span class="lj lj-plus-circle"></span> 
                                 <span class="small-text">Nuevo proyecto</span>
                             </button>
@@ -126,80 +126,8 @@
                                 <label class="habits-list__text">
                                     <input type="checkbox"> 
                                     <p>
-                                        <span class="normal-text">Lavar mi cara y usar crema facial</span> 
-                                        <span class="small-text">Mañana y noche</span>
-                                    </p>
-                                </label>
-                            </li>
-                            <li>
-                                <label class="habits-list__text">
-                                    <input type="checkbox"> 
-                                    <p>
-                                        <span class="normal-text">usar protector solar</span> 
-                                        <span class="small-text">Mañana</span>
-                                    </p>
-                                </label>
-                            </li>
-                            <li>
-                                <label class="habits-list__text">
-                                    <input type="checkbox"> 
-                                    <p>
                                         <span class="normal-text">Hacer ejercicio por 30 minutos</span> 
                                         <span class="small-text">Mañana</span>
-                                    </p>
-                                </label>
-                            </li>
-                            <li>
-                                <label class="habits-list__text">
-                                    <input type="checkbox"> 
-                                    <p>
-                                        <span class="normal-text">Comer frutas y verduras</span> 
-                                        <span class="small-text">Todo el día</span>
-                                    </p>
-                                </label>
-                            </li>
-                            <li>
-                                <label class="habits-list__text">
-                                    <input type="checkbox"> 
-                                    <p>
-                                        <span class="normal-text">Lavar  mis dientes 3 veces</span> 
-                                        <span class="small-text">Todo el día</span>
-                                    </p>
-                                </label>
-                            </li>
-                            <li>
-                                <label class="habits-list__text">
-                                    <input type="checkbox"> 
-                                    <p>
-                                        <span class="normal-text">Usar hilo dental por la noche</span> 
-                                        <span class="small-text">Noche</span>
-                                    </p>
-                                </label>
-                            </li>
-                            <li>
-                                <label class="habits-list__text">
-                                    <input type="checkbox"> 
-                                    <p>
-                                        <span class="normal-text">Realizar una actividad relajante</span> 
-                                        <span class="small-text">Todo el día</span>
-                                    </p>
-                                </label>
-                            </li>
-                            <li>
-                                <label class="habits-list__text">
-                                    <input type="checkbox"> 
-                                    <p>
-                                        <span class="normal-text">Lavar mis pies y limpiar mis uñas</span> 
-                                        <span class="small-text">Noche</span>
-                                    </p>
-                                </label>
-                            </li>
-                            <li>
-                                <label class="habits-list__text">
-                                    <input type="checkbox"> 
-                                    <p>
-                                        <span class="normal-text">Domir 8 horas</span> 
-                                        <span class="small-text">Noche</span>
                                     </p>
                                 </label>
                             </li>
@@ -213,40 +141,45 @@
     <div class="modal-overlay closed" id="modal-overlay"></div>
 
     <div class="modal closed" id="modal">
-        <div class="modal-guts">
-            <div class="modal-header">
+        <button class="close-button" id="close-button">X</button>
+        <div class="modal-guts" id="modal-data">
+            <!-- <div class="modal-header">
                 <p class="modal-title">Crear nuevo proyecto</p>
                 <button class="close-button" id="close-button">X</button>
             </div>
             <div class="modal-content">
-                <form class="modal-form">
+                <form id="formCreateProject" class="modal-form">
                     <div class="form-left">
                         <div class="form-group">
                             <label for="project-name">Nombre:</label>
-                            <input id="project-name" type="text" placeholder="Proyecto">
+                            <input class="input-project" id="project-name" type="text" placeholder="Proyecto" autocomplete="off">
                         </div>
                         <div class="form-group">
                             <textarea name="project-description" id="project-description" cols="10" rows="2" placeholder="Descripción del proyecto"></textarea>
                         </div>
                         <div class="form-group select-tags">
-                            <label for="project-name">Metas</label>
-                            <input id="project-name" type="text" placeholder="Crear metas">
+                            <label for="inputGoal">Metas</label>
+                            <div class="create-goals">
+                                <input class="input-goal" id="inputGoal" type="text" placeholder="Crear metas" autocomplete="off">
+                                <a id="createGoal" class="add-goal">+</a>
+                            </div>
+                            <div class="create-goals">
+                                <div class="create-goals-list"></div>
+                            </div>
                         </div>
                     </div>
                     <div class="form-right">
                         <div class="form-right-title">
                             <p>Atributos</p>
-                            <span class="lj lj-clock"></span>
+                            <span class="lj lj-bookmark"></span>
                         </div>
                         <div class="form-group">
                             <label class="attributes" for="project-status">Estado</label>
                             <select name="project-status" id="project-status">
                                 <option disabled="disabled" selected value="0">Estado</option>
-                                <option value="Activo">Activo</option>
-                                <option value="Inactivo">Inactivo</option>
                                 <option value="Pendiente">Pendiente</option>
-                                <option value="En proceso">En proceso</option>
-                                <option value="Alcanzado">Alcanzado</option>
+                                <option value="En progreso">Inactivo</option>
+                                <option value="Completado">Completado</option>
                             </select>
                         </div>
                         <div class="form-group">
@@ -259,12 +192,18 @@
                         </div>
                         <div class="form-group select-tags">
                             <label class="attributes" for="project-tags">Etiquetas</label>
-                            <textarea type="text" name="project-tags" id="project-tags" ></textarea>
+                            <div class="create-goals">
+                                <input class="input-goal" id="inputTag" type="text" placeholder="Agregar etiquetas" autocomplete="off">
+                                <a id="createTag" class="add-goal">+</a>
+                            </div>
+                            <div class="create-goals">
+                                <div class="create-tags-list"></div>
+                            </div>
                         </div>
                         <input class="form-btn" id="saveObjective" type="submit" value="Guardar">
                     </div>
                 </form>
-            </div>
+            </div> -->
         </div>
     </div>
 
